4f9c79a92a6afa5dbb740e2f0cd71d2473b11d75
Vitor Alves de Jesus <92615117+vitor29alves@users.noreply.github.com>
2022-09-17 10:23:02 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Button } from 'react-native';
+
+
+import Home from './src/page/home/index'
+
+import PhoneSignIn from './src/page/sms/index';
+
+
+export default function App() {
+
+  
+ 
+  return (
+
+
+    <SafeAreaView>
+
+      <Home />
+
+    </SafeAreaView>
+
+
+  );
+}
+
